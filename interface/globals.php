c38cdc23c813153d4194a2e47919898d1ce61b03
Ryan Nix <r-nix@northwestern.edu>
2020-09-30 08:53:47 -0500

    Another attempt to fix globals.php

--- interface/globals.php
+++ interface/globals.php
@@ -83,7 +83,7 @@ if (preg_match("/^[^\/]/", $web_root)) {
 // The webserver_root and web_root are now automatically collected in
 //  real time per above code. If above is not working, can uncomment and
 //  set manually here:
-$webserver_root = "/var/www/localhost/htdocs";
+$webserver_root = "/var/www/localhost/htdocs/openemr";
 $web_root =  "/openemr";
 
 // Debug function. Can expand for longer trace or file info.
